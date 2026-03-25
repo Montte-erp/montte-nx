52fb74aafa6afc3c6c3fc29108af6814053566c0
Manoel <manoel.carvalhoneto@tutamail.com>
2026-03-25 14:33:35 -0300

    feat(ui): register GlobalDialogStack in root layout

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -13,6 +13,7 @@ import {
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 import { GlobalAlertDialog } from "@/hooks/use-alert-dialog";
 import { GlobalCredenza } from "@/hooks/use-credenza";
+import { GlobalDialogStack } from "@/hooks/use-dialog-stack";
 import {
    PostHogWrapper,
    PosthogRouterTracker,
@@ -85,6 +86,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
                   <Toaster richColors />
                   <GlobalCredenza />
                   <GlobalAlertDialog />
+                  <GlobalDialogStack />
                   <ClientOnly>
                      <TanStackDevtools
                         config={{
