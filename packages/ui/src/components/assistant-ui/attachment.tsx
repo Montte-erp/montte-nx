a365314376734a9b332fd2df7c2bf0a6bbc3be17
Manoel <manoel.carvalhoneto@tutamail.com>
2026-04-11 15:21:22 -0300

    chore: Remove unused UI components

--- packages/ui/src/components/assistant-ui/attachment.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import type { FC } from "react";
-
-export const UserMessageAttachments: FC = () => null;
-export const ComposerAttachments: FC = () => null;
-export const ComposerAddAttachment: FC = () => null;
