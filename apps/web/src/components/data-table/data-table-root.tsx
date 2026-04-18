8846b4a7bfb6acec45e78fbbdeb0ff1ba4ac2b35
Manoel <manoel.carvalhoneto@tutamail.com>
2026-04-18 20:02:24 -0300

    feat(data-table): Conditionally render actions column pinning

--- apps/web/src/components/data-table/data-table-root.tsx
+++ apps/web/src/components/data-table/data-table-root.tsx
@@ -344,7 +344,7 @@ export function DataTableRoot<TData, TValue>({
          sorting,
          columnPinning: {
             left: ["__select"],
-            right: ["__actions"],
+            right: renderActions ? ["__actions"] : [],
          },
       },
    });
