adc33b868f1428ab50155e39ad8dae733ec729e2
Manoel <manoel.carvalhoneto@tutamail.com>
2026-05-05 10:26:28 -0300

    fix(web): group email OTP inputs

--- apps/web/src/routes/auth/email-verification.tsx
+++ apps/web/src/routes/auth/email-verification.tsx
@@ -8,7 +8,6 @@ import {
 import {
    InputOTP,
    InputOTPGroup,
-   InputOTPSeparator,
    InputOTPSlot,
 } from "@packages/ui/components/input-otp";
 import { useForm } from "@tanstack/react-form";
@@ -145,14 +144,10 @@ function EmailVerificationPage() {
                                     <InputOTPGroup>
                                        <InputOTPSlot index={0} />
                                        <InputOTPSlot index={1} />
-                                    </InputOTPGroup>
-                                    <InputOTPSeparator />
-                                    <InputOTPGroup>
                                        <InputOTPSlot index={2} />
-                                       <InputOTPSlot index={3} />
                                     </InputOTPGroup>
-                                    <InputOTPSeparator />
                                     <InputOTPGroup>
+                                       <InputOTPSlot index={3} />
                                        <InputOTPSlot index={4} />
                                        <InputOTPSlot index={5} />
                                     </InputOTPGroup>
